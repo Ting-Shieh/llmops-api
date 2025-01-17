19db0d60c9f7b2feb8f780b157e2d5b148a0063e
Ting-Shieh <zsting29@gmail.com>
2025-01-17 22:37:55 +0800

    Flask-Migrate 擴展與應用

--- app/http/app.py
+++ app/http/app.py
@@ -6,12 +6,14 @@
 @File   : app.py
 """
 import dotenv
+from flask_migrate import Migrate
 from injector import Injector
 
+from app.http import ExtensionModule
 from config import Config
 from internal.router import Router
 from internal.server import Http
-from module import ExtensionModule
+# from module import ExtensionModule
 from pkg.sqlalchemy import SQLAlchemy
 
 # 將.env 加載到環境變量中
@@ -20,7 +22,13 @@ dotenv.load_dotenv()
 conf = Config()
 
 injector = Injector([ExtensionModule])
-app = Http(__name__, conf=conf, db=injector.get(SQLAlchemy), router=injector.get(Router))
+app = Http(
+    __name__,
+    conf=conf,
+    db=injector.get(SQLAlchemy),
+    migrate=injector.get(Migrate),
+    router=injector.get(Router)
+)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
---
+++ internal/extension/migrate_extension.py
@@ -0,0 +1,10 @@
+#!/user/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Time   : 2025/1/17 下午9:34
+@Author : zsting29@gmail.com
+@File   : migrate_extension.py
+"""
+from flask_migrate import Migrate
+
+migrate = Migrate()
